a854a7625c00579cd9cb0e695e3b86219d503bf8
knoj014 <jongkyung2005@gmail.com>
2024-11-19 20:04:30 +0900

    aws

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -3,26 +3,26 @@ import './MyButton.css';
 import type React from 'react';
 
 interface MyButtonProps {
-    url: string;
-    logoSrc: string;
-    siteName: string;
+  url: string;
+  logoSrc: string;
+  siteName: string;
 }
 
-const MyButton: React.FC<MyButtonProps> = ({url, logoSrc, siteName}) => {
-    const handleRedirect = () => {
-        window.open(url, '_blank');
-      };
-    
-      return (
-        <button onClick={handleRedirect} className="button">
-          <img 
-            src = {logoSrc} // 로고 이미지 URL
-            alt={'${siteName}-logo'}
-            className="logo"
-          />
-          <span className="text"> {siteName} </span>
-        </button>
-      );
-}
+const MyButton: React.FC<MyButtonProps> = ({ url, logoSrc, siteName }) => {
+  const handleRedirect = () => {
+    window.open(url, '_blank');
+  };
+
+  return (
+    <button onClick={handleRedirect} className="button">
+      <img
+        src={logoSrc} // 로고 이미지 URL
+        alt={'${siteName}-logo'}
+        className="logo"
+      />
+      <span className="text"> {siteName} </span>
+    </button>
+  );
+};
 
 export default MyButton;
